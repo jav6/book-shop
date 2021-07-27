90414e537d5f330ff2f149ef1166639df544b8c1
Mohammad Javidi <m.javidii@yahoo.com>
2021-07-27 14:58:05 +0430

    Add view for show number of the book details -> in index of site

--- book/views.py
+++ book/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+from .models import Author, Book, BookInstance, Genre
+
 def index(request):
-    context = {}
+    num_books = Book.objects.all().count()
+    num_instance = BookInstance.objects.all().count()
+    num_instance_available = BookInstance.objects.filter(status__exact = 'a').count()
+    num_author = Author.objects.count()
+    context = {
+        'num_books': num_books,
+        'num_instance': num_instance,
+        'num_instance_available': num_instance_available,
+        'num_author': num_author,
+    }
     return render(request, 'book/index.html', context)
