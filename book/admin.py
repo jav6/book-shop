77015c6153b6d1cd9e629a1e61551dc677bfec56
Mohammad Javidi <m.javidii@yahoo.com>
2021-07-27 09:03:12 +0430

    Register : models to admin for manage site data

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Book, BookInstance, Genre
+
+@admin.register(Author)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+# @admin.register(BookInstance)
+# @admin.register(Genre)
