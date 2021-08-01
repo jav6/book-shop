10c56f7a4cdf16555235082a78963f1263006cb1
Mohammad Javidi <m.javidii@yahoo.com>
2021-08-01 21:26:26 +0430

    Write - url:route to book detail list view

--- book/urls.py
+++ book/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'book'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('list/', views.BookListView.as_view(), name='list'),
 ]
