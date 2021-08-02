07dbd32c2228d37c6dea413966e3130761ce440d
Mohammad Javidi <m.javidii@yahoo.com>
2021-08-02 08:56:24 +0430

    Add route for detail page url

--- book/urls.py
+++ book/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'book'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('list/', views.BookListView.as_view(), name='list'),
+    path('list/', views.BookListView.as_view(), name='bookList'),
+    path('detail/<int:pk>', views.BookDetailView.as_view(), name='bookDetail'),
 ]
