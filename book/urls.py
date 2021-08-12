994577bff58af29d96372e83e120ec4dd16e6f34
Mohammad Javidi <m.javidii@yahoo.com>
2021-08-12 10:44:14 +0430

    Add - url to search page

--- book/urls.py
+++ book/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('list/', views.BookListView.as_view(), name='bookList'),
     path('detail/<int:pk>', views.BookDetailView.as_view(), name='bookDetail'),
-    path('author/', views.AuthorListView.as_view(), name='authorList')
+    path('author/', views.AuthorListView.as_view(), name='authorList'),
+    path('search/', views.SearchResultsView.as_view(), name='search_result')
 ]
